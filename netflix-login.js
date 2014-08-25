af85ffefee341ed4c899f7d9f6c643de77c3ffff
Dorian Karter <dkarter@gmail.com>
2014-08-24 21:23:51 -0500

    API app complete need to add a readme and perhaps some tests

---
+++ netflix-login.js
@@ -0,0 +1,35 @@
+var casper = require('casper').create();
+var fs = require('fs');
+var utils = require('utils');
+
+var data = fs.read('credentials.json');
+var credentials = JSON.parse(data);
+loginToNetflix(credentials.username, credentials.password);
+
+
+function loginToNetflix (username, password) {
+	casper.start('https://www.netflix.com/Login?locale=en-US', function() {
+		this.echo('Attempting login to Netflix - filling and submitting login form');
+
+		this.fill('form#login-form', {
+				'email':    username,
+			 'password':    password,
+		   'RememberMe': 	true
+		}, true);
+
+	});
+
+	casper.then(function() {
+		this.echo('Login Complete - Please check the cookie as defined in command line');
+
+		var cookies = JSON.stringify(phantom.cookies);
+		fs.write("netflix-cookies.txt", cookies, 644);
+	});
+
+	casper.run(function () {
+		this.echo('Done - Exiting!');
+		casper.exit();
+	});
+	
+}
+
---
+++ server.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var request = require('request');
+var tough = require('tough-cookie');
+var sys = require('sys');
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+var cookieFile = 'netflix-cookies.txt';
+var noop = function () {};
+var apiDomain = 'api-global.netflix.com';
+var apiUrl = 'http://' + apiDomain;
+
+//TODO: add refresh flag to commandline - check it here and erase existing cookie
+if (!fs.existsSync(cookieFile)) {
+	var netflixLogin = exec('casperjs netflix-login.js', function (error, stdout, stderr) {
+		if (error) throw error;
+		readCookieFile();
+	});
+} else {
+	readCookieFile();
+}
+
+function readCookieFile () {
+	fs.readFile(cookieFile, function(error, data){
+		if (error) throw error;
+		var jar = new tough.CookieJar();
+
+		var cookies = JSON.parse(data);
+		for (var i = 0; i < cookies.length; i++) {
+			cookies[i].key = cookies[i].name;
+			cookies[i].domain = apiDomain;
+			var cookie = tough.Cookie.fromJSON(JSON.stringify(cookies[i]));
+			jar.store.putCookie(cookie, noop);
+		}
+		
+		serveAPI(jar);
+	});	
+}
+
+function serveAPI (cookieJar) {
+	//configure roots and serve app
+	var app = express();
+	
+	app.get('/autocomplete/:query', function (req, res) {
+		var autocompleteAPI = apiUrl + '/desktop/search/autocomplete?term=';
+		autocompleteAPI += req.params.query;
+		request({
+			url: autocompleteAPI,
+			headers: {
+				'Cookie': cookieJar.getCookieStringSync(apiUrl, {secure: false})
+			}
+		
+		}, function (error, response, json) {
+			if (error) 
+				res.send(error);
+			else
+				res.json(json);
+		});
+
+	});
+
+	app.listen('8080');
+
+	console.log('go to http://localhost:8080/autocomplete');
+
+	exports = module.exports = app;	
+}
+
