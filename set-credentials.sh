cb31bb2b7a6b04d9969a46588315be51a1876058
Dorian Karter <dkarter@gmail.com>
2014-08-28 01:18:27 -0500

    added a bash script to set the credentials

---
+++ set-credentials.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This is a script to write the credentials.json file using user input
+echo "Type your Netflix user name:"
+read username
+
+echo "Type your Netflix user password:"
+read password
+
+cat > credentials.json << endoffile
+{
+	"user": "$username",
+	"pass": "$password"
+}
+endoffile
